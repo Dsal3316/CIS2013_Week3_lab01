631ce481d1bb39a78394bbe9970509e68894eb04
Dsal3316 <35541000+Dsal3316@users.noreply.github.com>
2018-01-31 20:13:27 -0600

    Example visual studio

--- CIS2013_Week3_lab01/Flowcontrol.cpp
+++ CIS2013_Week3_lab01/Flowcontrol.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 
 int main() {
+	char continueProgram = 'y';
+	
+	do {
+		cout << "Do you want to continue (y/n)" << endl;
+		cin >> continueProgram;
+	
+	
+		switch (continueProgram) {
+		case 'y':
+			cout << "Going to continue" << endl;
+			break;
+		case 'n':
+			cout << "Going to exit" << endl;
+			break;
+		default:
+			continueProgram = 'y';
+		}
+	
+	} while (continueProgram == 'y');
 
+	return 0;
 }
